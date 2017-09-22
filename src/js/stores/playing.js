9c70e4db7fdda8ce3d4efe6d826e45ffaa522db3
trazyn <var.darling@gmail.com>
2017-09-22 18:08:17 +0800

    search in playlist

--- src/js/stores/playing.js
+++ src/js/stores/playing.js
@@ -1,11 +1,35 @@
 
 import { observable, action } from 'mobx';
+import han from 'han';
+
+import controller from './controller';
 
 class Playing {
     @observable show = false;
+    @observable filtered = [];
 
     @action toggle(show = !self.show) {
         self.show = show;
+    }
+
+    @action filter(text = '') {
+        var songs = [];
+
+        // Convert text to chinese pinyin
+        text = han.letter(text.trim());
+
+        songs = controller.playlist.songs.filter(e => {
+            return false
+                // Fuzzy match the song name
+                || han.letter(e.name).indexOf(text) > -1
+                // Fuzzy match the album name
+                || han.letter(e.album.name).indexOf(text) > -1
+                // Mathc the artist name
+                || e.artists.findIndex(e => han.letter(e.name).indexOf(text) > -1) !== -1
+            ;
+        });
+
+        self.filtered = songs;
     }
 }
 
