2c78c40a247ff2d84d24c85222e116bf308d9264
trazyn <tn.razy@gmail.com>
2017-09-26 15:38:12 +0800

    custome playlist background

--- src/js/pages/Preferences/classes.js
+++ src/js/pages/Preferences/classes.js
@@ -1,50 +1,90 @@
 
-export default theme => ({
-    container: {
-        padding: 32,
-        background: '#fff',
-        color: '#333',
+import colors from 'utils/colors';
 
-        '& header': {
-            fontFamily: 'HelveticaNeue-UltraLight',
-            fontSize: 24,
-            fontWeight: '100',
-            color: '#000',
-            letterSpacing: 1,
-            wordSpacing: 3,
-        },
+export default theme => {
+    return {
+        container: {
+            padding: 32,
+            background: '#fff',
+            color: '#333',
+            overflow: 'hidden',
+            overflowY: 'auto',
+            height: '100vh',
 
-        '& h4': {
-            margin: 0,
-            fontWeight: 'normal',
+            '& header': {
+                fontFamily: 'HelveticaNeue-UltraLight',
+                fontSize: 24,
+                fontWeight: '100',
+                color: '#000',
+                letterSpacing: 1,
+                wordSpacing: 3,
+            },
 
-            '& + p': {
+            '& h4': {
                 margin: 0,
-                marginTop: 2,
-                fontSize: 12,
-                color: 'gray',
+                fontWeight: 'normal',
+
+                '& + p': {
+                    margin: 0,
+                    marginTop: 2,
+                    fontSize: 12,
+                    color: 'gray',
+                }
+            },
+
+            '& h3': {
+                marginTop: 32,
+                fontFamily: 'HelveticaNeue-UltraLight',
+                fontSize: 18,
+                fontWeight: '100',
+                color: '#000',
+                letterSpacing: 1,
+                wordSpacing: 3,
+            },
+
+            '& section': {
+                fontFamily: 'Roboto',
+                fontSize: 14,
+                padding: 32,
+            },
+
+            '& label': {
+                display: 'flex',
+                margin: '24px 0',
+                justifyContent: 'space-between',
+                alignItems: 'center',
             }
         },
 
-        '& section': {
-            fontFamily: 'Roboto',
-            fontSize: 14,
-            padding: 32,
+        close: {
+            position: 'absolute',
+            height: 32,
+            top: 32,
+            right: 32,
+            cursor: 'pointer',
         },
 
-        '& label': {
-            display: 'flex',
+        background: {
             margin: '24px 0',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-        }
-    },
 
-    close: {
-        position: 'absolute',
-        height: 32,
-        top: 32,
-        right: 32,
-        cursor: 'pointer',
-    },
-});
+            '& input': {
+                display: 'block',
+                height: 36,
+                width: '100%',
+                lineHeight: '36px',
+                background: 'none',
+                color: 'gray',
+                fontSize: 13,
+                textAlign: 'left',
+                border: 0,
+                borderBottom: '1px solid #ddd',
+                outline: 0,
+                transition: '.2s',
+            },
+
+            '& input:focus': {
+                borderBottomColor: colors.pallet.coral,
+            },
+        }
+    };
+};
