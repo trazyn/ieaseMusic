a0a0a09485d61ba851d0db8be36ecf0e8256cd3c
trazyn <var.darling@gmail.com>
2017-09-26 14:27:57 +0800

    add preference option

--- src/js/stores/preferences.js
+++ src/js/stores/preferences.js
@@ -2,17 +2,25 @@
 import { observable, action } from 'mobx';
 import { ipcRenderer } from 'electron';
 
+import controller from './controller';
 import storage from 'utils/storage';
 
 class Preferences {
+    @observable showTray = true;
     @observable alwaysOnTop = true;
     @observable showNotification = true;
     @observable autoPlay = true;
 
     @action async init() {
         var preferences = await storage.get('preferences');
-        var { alwaysOnTop, showNotification, autoPlay } = preferences;
+        var {
+            showTray = self.showTray,
+            alwaysOnTop = self.alwaysOnTop,
+            showNotification = self.showNotification,
+            autoPlay = self.autoPlay
+        } = preferences;
 
+        self.showTray = !!showTray;
         self.alwaysOnTop = !!alwaysOnTop;
         self.showNotification = !!showNotification;
         self.autoPlay = !!autoPlay;
@@ -22,17 +30,25 @@ class Preferences {
     }
 
     @action async save() {
-        var { alwaysOnTop, showNotification, autoPlay } = self;
+        var { showTray, alwaysOnTop, showNotification, autoPlay } = self;
 
         await storage.set('preferences', {
+            showTray,
             alwaysOnTop,
             showNotification,
             autoPlay,
         });
 
         ipcRenderer.send('update-preferences', {
+            playing: controller.playing,
+            showTray,
             alwaysOnTop,
         });
+    }
+
+    @action setShowTray(showTray) {
+        self.showTray = showTray;
+        self.save();
     }
 
     @action setAlwaysOnTop(alwaysOnTop) {
