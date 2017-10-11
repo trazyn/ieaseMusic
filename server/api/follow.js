9cee2347ced2c4784032e51cd4ea624762db945c
trazyn <tn.razy@gmail.com>
2017-10-11 10:44:05 +0800

    follow user

---
+++ server/api/follow.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express()
+const { createWebAPIRequest } = require('../../NeteaseCloudMusicApi/util/util')
+
+router.get('/', (req, res) => {
+  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+  const id = req.query.id
+  const csrf = req.cookies['__csrf']
+  const data = {
+    csrf_token: csrf,
+    followId: id,
+  }
+
+  createWebAPIRequest(
+    'music.163.com',
+    `/weapi/user/follow/${id}?csrf_token=${csrf}`,
+    'POST',
+    data,
+    cookie,
+    music_req => {
+      res.send(music_req)
+    },
+    err => res.status(502).send('fetch error')
+  )
+})
+
+module.exports = router
