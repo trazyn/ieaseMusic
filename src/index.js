ff04e61a18e881e7cd3aecc3a291da88edd43a78
trazyn <var.darling@gmail.com>
2018-07-02 23:16:20 +0800

    Disable zooming

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 
+import { webFrame } from 'electron';
 import React from 'react';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'react-jss';
 
 import App from './app';
 import theme from './theme';
+
+webFrame.setVisualZoomLevelLimits(1, 1);
+webFrame.setLayoutZoomLevelLimits(0, 0);
 
 render(
     <ThemeProvider theme={theme}>
