d593bcf147f389e52260c1d833d4041a8c688d3f
unreal0 <unreal0@sina.cn>
2018-10-14 20:17:22 +0800

    修改user页面排版

--- src/js/pages/User/classes.js
+++ src/js/pages/User/classes.js
@@ -1,14 +1,59 @@
+import colors from 'utils/colors';
 
 export default theme => ({
+    // container: {
+    //     height: '100vh',
+    //     background: 'rgba(255, 255, 255, .3)',
+    // },
     container: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: '100vw',
         height: '100vh',
         background: 'rgba(255, 255, 255, .3)',
+
+    },
+
+    navs: {
+        paddingLeft: 50,
+        paddingTop: 80,
+        fontWeight: '300',
+        color: 'white',
+
+        '& a': {
+            color: 'inherit !important',
+        },
+
+        '& nav a:after': {
+            content: '""',
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            height: 20,
+            width: 0,
+            background: colors.pallet.dribbble,
+            transform: 'translateY(10px)',
+            opacity: 0,
+            transition: '.2s ease-out',
+            zIndex: -1,
+        },
+
+        '& nav a:hover:after, & nav $playing:after': {
+            opacity: 1,
+            width: '110%',
+        },
+
+        '& nav p': {
+            margin: 0,
+            marginBottom: 24,
+        },
     },
 
     hero: {
-        display: 'flex',
+        // display: 'flex',
         height: 260,
-        justifyContent: 'flex-start',
+        // justifyContent: 'flex-start',
         alignItems: 'center',
 
         '& figure': {
@@ -56,7 +101,8 @@ export default theme => ({
     followed: {},
 
     info: {
-        paddingTop: 40,
+        paddingTop: 30,
+        paddingLeft: 30
     },
 
     username: {
@@ -78,7 +124,7 @@ export default theme => ({
 
         '& span': {
             display: 'inline-block',
-            padding: '8px 0',
+            padding: '8px 30px',
             fontSize: 12,
             textTransform: 'uppercase',
             color: '#000',
@@ -88,17 +134,17 @@ export default theme => ({
     },
 
     signature: {
-        marginTop: 34,
+        marginTop: 14,
         fontSize: 14,
         transform: 'translate(-20px, 20px)',
 
         '& span': {
-            display: '-webkit-box',
+            // display: '-webkit-box',
             padding: '16px 30px',
-            maxWidth: 280,
+            maxWidth: 230,
             lineHeight: '32px',
             color: '#000',
-            border: '2px solid #000',
+            // border: '2px solid #000',
             whiteSpace: 'normal',
             overflow: 'hidden',
             textOverflow: 'ellipsis',
@@ -108,61 +154,146 @@ export default theme => ({
         }
     },
 
+    // list: {
+    //     marginTop: 20,
+    // },
     list: {
-        marginTop: 20,
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        width: 480,
+        paddingLeft: 0,
+        height: 'calc(100vh - 0px)',
+        overflow: 'hidden',
+        overflowY: 'auto',
     },
-
     item: {
         position: 'relative',
-        fontSize: 14,
-        cursor: 'pointer',
-        transition: '.4s',
+        height: 100,
+        padding: 0,
+        margin: 0,
+        // backgroundImage: 'linear-gradient(110deg, rgb(255, 103, 0) 0%, rgb(255, 45, 240) 100%)',
+        fontFamily: 'Roboto',
+        fontWeight: 'lighter',
+        color: 'white',
+        overflow: 'hidden',
 
-        '& figure': {
-            width: 120,
-            height: 120,
-            margin: '50px 30px',
-            boxShadow: '0 0 24px 0 #333',
+        '& figcaption': {
+            position: 'absolute',
+            top: '50%',
+            left: 100,
+            marginTop: -25,
+            display: 'flex',
+            flexDirection: 'row',
+            zIndex: 2,
         },
 
-        '& p': {
-            padding: 0,
+        '& summary': {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            padding: '7px 0',
+            paddingLeft: 20,
+        },
+
+        '& summary p': {
             margin: 0,
-            transition: '1s',
-        },
-
-        '& $played': {
-            width: '0%',
+            fontSize: 14,
+            maxWidth: 180,
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
             overflow: 'hidden',
         },
 
-        '& p span': {
-            display: 'inline-block',
-            padding: '4px 8px',
-            background: '#000',
-            color: '#fff',
+        '& summary small': {
+            fontSize: 12,
+            color: '#eee',
         },
 
-        '&$playing, &:hover': {
-            transform: 'translateY(-30px)',
-        },
-
-        '&$playing p, &:hover p': {
-            visibility: 'visible',
-            opacity: 1,
-        },
-
-        '&$playing $played, &:hover $played': {
+        '&::after': {
+            content: '""',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            height: '100%',
             width: '100%',
+            background: 'rgba(255, 255, 255, .5)',
+            zIndex: 1
+        },
+    },
+    // item: {
+    //     position: 'relative',
+    //     fontSize: 14,
+    //     cursor: 'pointer',
+    //     transition: '.4s',
+
+    //     '& figure': {
+    //         width: 120,
+    //         height: 120,
+    //         margin: '50px 30px',
+    //         boxShadow: '0 0 24px 0 #333',
+    //     },
+
+    //     '& p': {
+    //         padding: 0,
+    //         margin: 0,
+    //         transition: '1s',
+    //     },
+
+    //     '& $played': {
+    //         width: '0%',
+    //         overflow: 'hidden',
+    //     },
+
+    //     '& p span': {
+    //         display: 'inline-block',
+    //         padding: '4px 8px',
+    //         background: '#000',
+    //         color: '#fff',
+    //     },
+
+    //     '&$playing, &:hover': {
+    //         transform: 'translateY(-30px)',
+    //     },
+
+    //     '&$playing p, &:hover p': {
+    //         visibility: 'visible',
+    //         opacity: 1,
+    //     },
+
+    //     '&$playing $played, &:hover $played': {
+    //         width: '100%',
+    //     },
+
+    //     '$played': {
+    //         visibility: 'hidden',
+    //         opacity: 0,
+    //         whiteSpace: 'nowrap',
+    //     }
+    // },
+    clearfix: {
+        position: 'relative',
+        display: 'block',
+
+        '&:hover $hovered': {
+            opacity: 1,
+            transform: 'scale(1)',
         },
 
-        '$played': {
-            visibility: 'hidden',
+        '&:hover [class^=Component-status-]': {
             opacity: 0,
-            whiteSpace: 'nowrap',
-        }
-    },
+            transform: 'scale(.8)',
+        },
 
+        '&:hover $background': {
+            transform: 'translateY(-50%) scale(1.1)',
+        },
+
+        '&:hover $album$background': {
+            transform: 'translateY(-8px) scale(1.1)',
+        },
+    },
     playing: {},
 
     meta: {
--- src/js/pages/User/index.js
+++ src/js/pages/User/index.js
@@ -1,8 +1,11 @@
+import React, {Component} from 'react';
+// import Scroller from 'react-scroll-horizontal';
+// import 'react-virtualized/styles.css';
+// import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+// import List from 'react-virtualized/dist/commonjs/List';
 
-import React, { Component } from 'react';
-import Scroller from 'react-scroll-horizontal';
-import { Link } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
+import {Link} from 'react-router-dom';
+import {inject, observer} from 'mobx-react';
 import injectSheet from 'react-jss';
 import clazz from 'classname';
 
@@ -11,6 +14,8 @@ import helper from 'utils/helper';
 import ProgressImage from 'ui/ProgressImage';
 import Loader from 'ui/Loader';
 import Header from 'components/Header';
+// import FadeImage from 'ui/FadeImage';
+import Controller from 'components/Controller';
 
 @inject(stores => ({
     loading: stores.user.loading,
@@ -19,182 +24,171 @@ import Header from 'components/Header';
     profile: stores.user.profile,
     playlists: stores.user.playlists,
     follow: stores.user.follow,
+    controller: stores.controller,
     isme: () => stores.user.profile.id === stores.me.profile.userId.toString(),
-    isPlaying: (id) => {
+    isPlaying: id => {
         var controller = stores.controller;
 
-        return controller.playing
-            && controller.playlist.id === id;
+        return controller.playing && controller.playlist.id === id;
     },
     naturalScroll: stores.preferences.naturalScroll,
 }))
 @observer
 class User extends Component {
-    componentWillMount = () => this.props.getUser(this.props.match.params.id);
+  componentWillMount = () => this.props.getUser(this.props.match.params.id);
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.match.params.id !== this.props.match.params.id) {
-            nextProps.getUser(nextProps.match.params.id);
-        }
-    }
+  componentWillReceiveProps(nextProps) {
+      if (nextProps.match.params.id !== this.props.match.params.id) {
+          nextProps.getUser(nextProps.match.params.id);
+      }
+  }
 
-    renderList() {
-        var { classes, playlists, naturalScroll } = this.props;
+  renderList() {
+      var {classes, playlists} = this.props;
 
-        return (
-            <Scroller reverseScroll={!naturalScroll}>
-                {
-                    () => (
-                        playlists.map(
-                            (e, index) => {
-                                return (
-                                    <Link
-                                        className={
-                                            clazz(
-                                                'clearfix',
-                                                classes.item,
-                                                {
-                                                    [classes.playing]: this.props.isPlaying(e.id)
-                                                }
-                                            )
-                                        }
-                                        to={e.link}
-                                        key={index}
-                                    >
-                                        <ProgressImage
-                                            {...{
-                                                height: 120,
-                                                width: 120,
-                                                src: e.cover,
-                                            }}
-                                        />
+      return (
+      //   <Scroller reverseScroll={!naturalScroll}>
+      //   <div>
+      //       {() =>
+          playlists.map((e, index) => {
+              return (
+                  <Link
+                      className={clazz('clearfix', classes.item, {
+                          [classes.playing]: this.props.isPlaying(e.id),
+                      })}
+                      to={e.link}
+                      key={index}
+                  >
+                      <ProgressImage
+                          {...{
+                              height: 120,
+                              width: 120,
+                              src: e.cover,
+                          }}
+                      />
 
-                                        <div className={classes.meta}>
-                                            <p className={classes.name}>
-                                                <span>{e.name}</span>
-                                            </p>
-                                            <p className={classes.played}>
-                                                <span>{helper.humanNumber(e.played)} Played</span>
-                                            </p>
-                                        </div>
-                                    </Link>
-                                );
-                            }
-                        )
-                    )
-                }
-            </Scroller>
-        );
-    }
+                      <div className={classes.meta}>
+                          <p className={classes.name}>
+                              <span> {e.name} </span>
+                          </p> <p className={classes.played}>
+                              <span>
+                                  {helper.humanNumber(e.played)}
+                                                    Played
+                              </span>
+                          </p>
+                      </div>
+                  </Link>
+              );
+          })
+      //         }
+      //   </div>
 
-    render() {
-        var { classes, loading, song, profile, isme, follow } = this.props;
-        var followed = profile.followed;
+      //   </Scroller>
+      );
+  }
 
-        // Force rerender all, let image progressively load
-        if (loading) {
-            return <Loader show={true} />;
-        }
+  render() {
+      var {classes, loading, song, profile, isme, follow, controller} = this.props;
+      var followed = profile.followed;
 
-        return (
-            <div className={classes.container}>
-                <Header
-                    {...{
-                        transparent: true,
-                        showBack: true,
-                        showPlaylist: true,
-                    }}
-                />
+      // Force rerender all, let image progressively load
+      if (loading) {
+          return <Loader show={true} />;
+      }
 
-                <button
-                    style={{
-                        display: isme() ? 'none' : 'block'
-                    }}
-                    className={
-                        clazz(classes.follow, {
-                            [classes.followed]: followed,
-                        })
-                    }
-                    onClick={e => follow(followed)}
-                >
-                    {
-                        followed ? 'Followed' : 'Follow'
-                    }
-                </button>
+      return (
+          <div className={classes.container}>
 
-                {
-                    song.id
-                        ? (
-                            <figure
-                                style={{
-                                    position: 'absolute',
-                                    left: 0,
-                                    top: 0,
-                                    height: '100%',
-                                    width: '100%',
-                                    padding: 0,
-                                    margin: 0,
-                                    overflow: 'hidden',
-                                }}
-                            >
-                                <figcaption
-                                    style={{
-                                        position: 'absolute',
-                                        left: 0,
-                                        top: 0,
-                                        width: window.innerWidth,
-                                        height: window.innerWidth,
-                                        padding: 0,
-                                        margin: 0,
-                                        backgroundImage: `url(${song.album.cover.replace(/\?param=.*/, '') + '?param=800y800'})`,
-                                        backgroundSize: `${window.innerWidth}px ${window.innerWidth}px`,
-                                        filter: 'blur(10px)',
-                                        zIndex: -1,
-                                    }}
-                                />
-                            </figure>
-                        )
-                        : false
-                }
+              <Header
+                  {...{
+                      transparent: true,
+                      showBack: true,
+                      showPlaylist: true,
+                  }}
+              />
+              <button
+                  style={{
+                      display: isme() ? 'none' : 'block',
+                  }}
+                  className={clazz(classes.follow, {
+                      [classes.followed]: followed,
+                  })}
+                  onClick={e => follow(followed)}
+              >
+                  {followed ? 'Followed' : 'Follow'}
+              </button>
+              {song.id
+                  ? <figure
+                      style={{
+                          position: 'absolute',
+                          left: 0,
+                          top: 0,
+                          height: '100%',
+                          width: '100%',
+                          padding: 0,
+                          margin: 0,
+                          overflow: 'hidden',
+                      }}
+                  >
+                      <figcaption
+                          style={{
+                              position: 'absolute',
+                              left: 0,
+                              top: 0,
+                              width: window.innerWidth,
+                              height: window.innerWidth,
+                              padding: 0,
+                              margin: 0,
+                              backgroundImage: `url(${song.album.cover.replace(/\?param=.*/, '') + '?param=800y800'})`,
+                              backgroundSize: `${window.innerWidth}px ${window.innerWidth}px`,
+                              filter: 'blur(10px)',
+                              zIndex: -1,
+                          }}
+                      />
+                  </figure>
+                  : false}
+              <main>
+                  <aside className={classes.navs}>
+                      <nav className={classes.menu}>
+                          <div className={classes.hero}>
+                              <ProgressImage
+                                  {...{
+                                      height: 160,
+                                      width: 160,
+                                      src: profile.avatar,
+                                  }}
+                              />
+                              {/* <FadeImage src={profile.avatarUrl} /> */}
+                              <div className={classes.info}>
+                                  <p className={classes.username}>
+                                      <span> {profile.name} </span>
+                                  </p>
 
-                <div className={classes.hero}>
-                    <ProgressImage
-                        {...{
-                            height: 260,
-                            width: 260,
-                            src: profile.avatar,
-                        }}
-                    />
+                                  <p className={classes.followers}>
+                                      <span>
+                                          {helper.formatNumber(profile.followers)}
+                                Followers
+                                      </span>
+                                  </p>
 
-                    <div className={classes.info}>
-                        <p className={classes.username}>
-                            <span>
-                                {profile.name}
-                            </span>
-                        </p>
+                                  <p className={classes.signature}>
+                                      <span> {profile.signature || 'No signature~'} </span>
+                                  </p>
+                              </div>
+                          </div>
+                      </nav>
+                  </aside>
+                  <section className={classes.list}>
 
-                        <p className={classes.followers}>
-                            <span>
-                                {helper.formatNumber(profile.followers)} Followers
-                            </span>
-                        </p>
+                      {this.renderList()}
+                  </section>
+                  {/* <div className={classes.list}> {this.renderList()} </div> */}
+              </main>
+              <Controller key={controller.song.id} />
 
-                        <p className={classes.signature}>
-                            <span>
-                                {profile.signature || 'No signature~'}
-                            </span>
-                        </p>
-                    </div>
-                </div>
-
-                <div className={classes.list}>
-                    {
-                        this.renderList()
-                    }
-                </div>
-            </div>
-        );
-    }
+          </div>
+      );
+  }
 }
 
 export default injectSheet(classes)(User);
