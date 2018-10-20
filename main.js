4e9e0d2c8f78c1f7746b322e7c797f90cea5c1ac
unreal0 <unreal0@sina.cn>
2018-10-20 17:35:33 +0800

    修复离在线问题

--- main.js
+++ main.js
@@ -628,6 +628,10 @@ const createMainWindow = () => {
     // Quit app
     ipcMain.on('goodbye', () => goodbye());
 
+    // A javaScript error occured in the main process
+    // process.on('uncaughtException', (_error) => {
+    //     console.error('an error happened');
+    // });
     // App has suspend
     powerMonitor.on('suspend', () => {
         mainWindow.webContents.send('player-pause');
--- src/js/ui/Offline/index.js
+++ src/js/ui/Offline/index.js
@@ -4,26 +4,45 @@ import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
 
 import classes from './classes';
-
 class Offline extends Component {
     static propTypes = {
         show: PropTypes.bool.isRequired,
     };
+    state = {
+        offline: false,
+    };
+    componentDidMount() {
+        window.addEventListener('offline', () => {
+            this.setState({
+                offline: true,
+            });
+        });
 
+        window.addEventListener('online', () => {
+            this.setState({
+                offline: false,
+            });
+        });
+    }
     render() {
         var { classes, show } = this.props;
-
         if (!show) {
             return false;
         }
+        if (!this.state.offline) {
+            return (
+                <div className={classes.container}>
+                    <h1>Opps, seems like you are offline...</h1>
+                    {/* <button onClick={e => window.location.reload()}>Reload</button> */}
+                </div>);
+        }
 
-        return (
-            <div className={classes.container}>
-                <h1>Opps, seems like you are offline...</h1>
-
-                <button onClick={e => window.location.reload()}>Reload</button>
-            </div>
-        );
+        // return (
+        //     <div className={classes.container}>
+        //         <h1>Opps, seems like you are offline...</h1>
+        //         {/* <online /> */}
+        //     </div>
+        // );
     }
 }
 
