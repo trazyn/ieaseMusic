b59ff221abe7cad4a6a086b16cf55dc4090676ca
trazyn <var.darling@gmail.com>
2019-01-26 16:43:02 +0800

    Clean log

--- server/router/home.js
+++ server/router/home.js
@@ -231,7 +231,6 @@ router.get('/:id?', async(req, res) => {
         let personalized = await getPersonalized();
 
         personalized[0].songs = await getSongs(personalized[0].id);
-        debug('%O', personalized[0].songs);
         list = [
             ...personalized,
             ...(await getNewest()),
